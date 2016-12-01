3d554d17c8bc4725e066fe1027fb0d240ae4003a
seis <xiaonanln@gmail.com>
2016-12-02 00:04:11 +0800

    convert

--- typeconv.go
+++ typeconv.go
@@ -1,6 +1,10 @@
 package typeconv
 
-import "github.com/Sirupsen/logrus"
+import (
+	"fmt"
+	"log"
+	"reflect"
+)
 
 func Int(v interface{}) int64 {
 	if n, ok := v.(uint64); ok {
@@ -17,13 +21,12 @@ func Int(v interface{}) int64 {
 
 	if n, ok := v.(float64); ok {
 		if float64(int64(n)) != n {
-			logrus.Panicf("Int: can not convert %v to int64", n)
+			log.Panicf("Int: can not convert %v to int64", n)
 		}
 
 		return int64(n)
 	}
-
-	logrus.Panicf("Int: can not convert: %T %v", v, v)
+	log.Panicf("Int: can not convert: %T %v", v, v)
 	return 0
 }
 
@@ -46,7 +49,7 @@ func IntTuple(v interface{}) []int64 {
 		}
 		return ret
 	}
-	logrus.Panicf("IntTuple: can not convert: %T %v", v, v)
+	log.Panicf("IntTuple: can not convert: %T %v", v, v)
 	return nil
 }
 
@@ -58,7 +61,7 @@ func Float(v interface{}) float64 {
 		return float64(f)
 	}
 
-	logrus.Panicf("Float: can not convert: %T %v", v, v)
+	log.Panicf("Float: can not convert: %T %v", v, v)
 	return 0.0
 }
 
@@ -82,10 +85,37 @@ func FloatTuple(v interface{}) []float64 {
 		return ret
 	}
 
-	logrus.Panicf("FloatTuple: can not convert: %T %v", v, v)
+	log.Panicf("FloatTuple: can not convert: %T %v", v, v)
 	return nil
 }
 
-func String(v interface{}) string{
+func String(v interface{}) string {
 	return v.(string)
+}
+
+// try to convert value to target type, panic if fail
+func Convert(val reflect.Value, targetType reflect.Type) reflect.Value {
+	valType := val.Type()
+	if valType.ConvertibleTo(targetType) {
+		return val.Convert(targetType)
+	}
+
+	//fmt.Printf("Value type is %v, emptyInterfaceType is %v, equals %v\n", valType, emptyInterfaceType, valType == emptyInterfaceType)
+	interfaceVal := val.Interface()
+
+	switch realVal := interfaceVal.(type) {
+	case float64:
+		return reflect.ValueOf(realVal).Convert(targetType)
+	case []interface{}:
+		// val is of type []interface{}, try to convert to typ
+		sliceSize := len(realVal)
+		targetSlice := reflect.MakeSlice(targetType, 0, sliceSize)
+		elemType := targetType.Elem()
+		for i := 0; i < sliceSize; i++ {
+			targetSlice = reflect.Append(targetSlice, Convert(val.Index(i), elemType))
+		}
+		return targetSlice
+	}
+
+	panic(fmt.Errorf("convert from type %v to %v failed: %v", valType, targetType, val))
 }
